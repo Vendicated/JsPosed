5601846b6d845646b53cf04cc39570ad10dab9dd
V <vendicated@riseup.net>
2023-05-26 21:21:59 +0200

    clean up tests

--- tests/index.js
+++ tests/index.js
@@ -22,41 +22,80 @@ const obj = {
 };
 obj.add.someProp = "hello";
 
-const patcher = new Patcher();
+/**
+ * @param {(patcher: Patcher) => void} task
+ */
+function stage(task) {
+    const patcher = new Patcher();
+    task(patcher);
+    patcher.unpatchAll();
+}
 
 assert.equal(obj.add(1, 2), 3);
 
 // TEST INSTEAD
-const unpatch = patcher.instead(obj, "add", (_) => 42);
-assert.equal(obj.add(1, 2), 42);
-// TEST RESTORES PROPS
-assert.equal(obj.add.someProp, "hello");
-unpatch.unpatch();
+stage(patcher => {
+    patcher.instead(obj, "add", (_) => 42);
+    assert.equal(obj.add(1, 2), 42);
+    // TEST RESTORES PROPS
+    assert.equal(obj.add.someProp, "hello");
+});
 
 // TEST PATCHING
-patcher.before(obj, "add", (param) => (param.args[0] = 42));
-assert.equal(obj.add(1, 2), 44);
-patcher.after(obj, "add", (param) => (param.result *= 2));
-assert.equal(obj.add(1, 2), 88);
+stage(patcher => {
+    patcher.before(obj, "add", (param) => (param.args[0] = 42));
+    assert.equal(obj.add(1, 2), 44);
+    patcher.after(obj, "add", (param) => (param.result *= 2));
+    assert.equal(obj.add(1, 2), 88);
+});
 
 // SHOULD NOT BREAK 'this'
-patcher.before(obj, "getAdd", (_) => _);
-assert.equal(obj.getAdd(), obj.add);
+stage(patcher => {
+    patcher.before(obj, "getAdd", (_) => _);
+    assert.equal(obj.getAdd(), obj.add);
+});
 
 // TEST CALL ORIGINAL
-assert.equal(patcher.callOriginal(obj.add, obj, 1, 2), 3);
+stage(patcher => {
+    patcher.instead(obj, "add", (param) => 42);
+    assert.equal(patcher.callOriginal(obj.add, obj, 1, 2), 3);
+});
 
 // TEST UNPATCHALL
-patcher.unpatchAll();
-assert.equal(obj.add(1, 2), 3);
+stage(patcher => {
+    patcher.instead(obj, "add", (param) => 42);
+    patcher.unpatchAll();
+    assert.equal(obj.add(1, 2), 3);
+});
 
 // TEST THROW
-patcher.after(obj, "add", (param) => (param.error = new Error("test")));
-assert.throws(() => obj.add(1, 2), { message: "test" });
+stage(patcher => {
+    patcher.after(obj, "add", (param) => (param.error = new Error("test")));
+    assert.throws(() => obj.add(1, 2), { message: "test" });
+});
 
 // TEST PRIORITY
-patcher.unpatchAll();
-patcher.instead(obj, "add", (param) => 1);
-patcher.instead(obj, "add", (param) => 2, PatchPriority.MAX);
-patcher.instead(obj, "add", (param) => 3);
-assert.equal(obj.add(1, 2), 2);
+stage(patcher => {
+    patcher.instead(obj, "add", (param) => 1);
+    patcher.instead(obj, "add", (param) => 2, PatchPriority.MAX);
+    patcher.instead(obj, "add", (param) => 3);
+    assert.equal(obj.add(1, 2), 2);
+});
+
+// TEST CUSTOM ERROR HANDLER
+stage(patcher => {
+    let passedTest = false;
+
+    const patcherWithCustomError = new Patcher("j", (kind, info, err) => {
+        passedTest = true;
+    });
+
+    patcherWithCustomError.before(obj, "add", param => {
+        throw new Error();
+    });
+
+    obj.add(1, 2);
+    assert(passedTest);
+
+    patcherWithCustomError.unpatchAll();
+});
